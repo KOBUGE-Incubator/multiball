014f729c0e67770d7e975abd86d5fce33508566c
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2015-04-30 23:22:45 +0300

    Small fixes

--- game/scripts/ball.gd
+++ game/scripts/ball.gd
@@ -6,7 +6,8 @@ export var rotation_speed = 20.0 # The speed of rotaion of the ball
 export var camera_path = NodePath("../camera") # The path to the camera
 var camera # The camera itself
 var camera_offset = Vector3(0, 3, 6) # The offset from the camera
-var rotation_y = 0
+var rotation_y = 0 # the rotation of the camera on y
+var rotation_y_speed = 0 # The speed of rotation on Y
 
 func _ready():
 	# Get needed nodes
@@ -15,9 +16,14 @@ func _ready():
 	set_fixed_process(true)
 	set_process(true)
 
+func set_color(color):
+	get_node("mesh").get_material_override().set_shader_param("Color",color)
+
 func _process(delta):
 	# Transform the offset, so it is in "local" coords
 	var matrix = get_transform().inverse().basis
+	rotation_y_speed = lerp(rotation_y_speed,0,delta*2)
+	rotation_y += rotation_y_speed*delta
 	var transform_matrix = Matrix3(Vector3(0, 1, 0), rotation_y)
 	var transformed_offset = transform_matrix.xform(camera_offset)
 	# Place the camera in the right place
@@ -35,10 +41,10 @@ func _fixed_process(delta):
 		force += Vector3(0, 0, speed) # Move backward
 	if(Input.is_action_pressed("rotate_right")):
 		force += Vector3(speed/2, 0, 0) # Move right
-		rotation_y += rotation_speed*delta
+		rotation_y_speed += rotation_speed*delta
 	if(Input.is_action_pressed("rotate_left")):
 		force += Vector3(-speed/2, 0, 0) # Move right
-		rotation_y -= rotation_speed*delta
+		rotation_y_speed -= rotation_speed*delta
 	# Transform the force, so it is in "camera" coords
 	var transform_matrix = Matrix3(Vector3(0, 1, 0), rotation_y)
 	force = transform_matrix.xform(force)
