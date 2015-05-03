cec5b33519255acdc1584113b5f07063d0d607d6
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2015-05-03 20:15:39 +0300

    Tweaks

--- game/scripts/ball.gd
+++ game/scripts/ball.gd
@@ -5,7 +5,8 @@ export var speed = 20.0 # The speed of the ball
 export var rotation_speed = 20.0 # The speed of rotaion of the ball
 export var rotation_carry = 1.0 # How much of the rotation speed is carried over?
 export var camera_path = NodePath("../camera") # The path to the camera
-export var mouse_scale = Vector2(2,3)
+export var mouse_scale = Vector3(2,3,0)
+export var mouse_motion_scale = Vector3(0,0,0)
 var camera # The camera itself
 var default_camera_offset = Vector3(0, 3, 5) # The default offset from the camera
 var camera_offset = default_camera_offset # The offset from the camera
@@ -17,7 +18,7 @@ var start_pos = Vector3(-1.5,5,-1.5) # The starting position # TODO
 var should_respawn = false # Should we respawn?
 var phisics_space # the phisics space, used to perform raycasts
 var have_not_seen_ball_from = 0.0 # How much time passed from the last time we saw that ball
-var mouse_pos = Vector2(0,0) # The position of the mouse
+var mouse_pos = Vector3(0,0,0) # The position of the mouse
 
 func _ready():
 	# Get needed nodes and stuff
@@ -48,13 +49,13 @@ func _input(event):
 	if(event.is_action("respawn") && event.is_pressed() && !event.is_echo()):
 		should_respawn = true
 	if(event.type == InputEvent.MOUSE_MOTION):
-		mouse_pos = (event.pos / OS.get_window_size() - Vector2(0.5,0.5)) * mouse_scale
+		mouse_pos = (event.pos / OS.get_window_size() - Vector2(0.5,0.5))
 
 func _process(delta):
 	# Transform the offset, so it is in "local" coords
 	# Place the camera in the right place
 	var transform_matrix = Matrix3(Vector3(0, 1, 0), rotation_y)
-	camera.set_translation(target_camera_pos + transform_matrix.xform(Vector3(mouse_pos.x, mouse_pos.y, 0)))
+	camera.set_translation(target_camera_pos)
 	# Rotate the camera, so it looks at the ball
 	var current_transform = camera.get_transform()
 	current_transform = current_transform.looking_at(get_translation(), Vector3(0, 1, 0))
@@ -68,27 +69,29 @@ func _fixed_process(delta):
 	var transformed_offset = transform_matrix.xform(camera_offset)
 	camera_offset = camera_offset.linear_interpolate(target_camera_offset, delta*3)
 	rotation_y_speed = lerp(rotation_y_speed,0,delta*2)
-	var new_target_camera_pos = get_translation() + transformed_offset
+	var new_target_camera_pos = get_translation() + transformed_offset + transform_matrix.xform(Vector3(mouse_pos.x, mouse_pos.y, 0) * mouse_scale)
 	target_camera_pos = target_camera_pos.linear_interpolate(new_target_camera_pos, delta*6)
 	if(should_respawn): # Must we respawn
 		should_respawn = false
 		respawn()
 		return # Don't calculate movement
-	var force = Vector3(0, 0, 0) # The force
+	var force = Vector3(mouse_pos.x, 0, mouse_pos.y)*mouse_motion_scale # The force 
 	if(Input.is_action_pressed("forward")):
 		force += Vector3(0, 0, -1) # Move forward
 	if(Input.is_action_pressed("back")):
 		force += Vector3(0, 0, 1) # Move backward
 	if(Input.is_action_pressed("rotate_right")):
-		force += Vector3(1/2, 0, 0) # Move right
-		rotation_y_speed += rotation_speed*delta + sqrt(abs(rotation_y_speed))/rotation_carry
+		force += Vector3(0.5, 0, 0) # Move right
 	if(Input.is_action_pressed("rotate_left")):
-		force += Vector3(-1/2, 0, 0) # Move right
-		rotation_y_speed -= rotation_speed*delta + sqrt(abs(rotation_y_speed))/rotation_carry
+		force += Vector3(-0.5, 0, 0) # Move right
+	
+	rotation_y_speed += (rotation_speed*delta + sqrt(abs(rotation_y_speed))/rotation_carry)*force.x # Rotate the camera
 	# Transform the force, so it is in "camera" coords
 	force = transform_matrix.xform(force)
+	if(force.length_squared() > 1):
+		force = force.normalized()
 	# Apply the forces
-	set_linear_velocity(get_linear_velocity() + force.normalized()*speed*delta)
+	set_linear_velocity(get_linear_velocity() + force*speed*delta)
 	target_camera_offset += force/2
 	target_camera_offset = target_camera_offset.linear_interpolate(default_camera_offset, delta*10)
 	# Check if we can see the ball, and if not, move the camera
