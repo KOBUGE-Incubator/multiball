d58e83b9914e84bdce0d5399ea253774f4c9eae0
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2016-10-24 21:03:53 +0300

    Add level selection in the menu

--- game/main/main.gd
+++ game/main/main.gd
@@ -7,27 +7,45 @@ var peer_players = {}
 var local_player = null
 var teams = []
 
-onready var level = get_node("level")
+var level = null
+var level_path = ""
 
 func _ready():
-	for team in get_children():
-		if team extends preload("res://player/team.gd"):
-			team.level = level
-			teams.push_back(team)
-	
 	print("Initialized, mode: ", get_tree().get_root().get_network_mode())
 	if is_network_master():
 		get_tree().get_meta("network_peer").connect("peer_connected", self, "peer_connected")
 		get_tree().get_meta("network_peer").connect("peer_disconnected", self, "peer_disconnected")
 
+slave func load_level(path):
+	level_path = path
+	if is_network_master():
+		rpc("load_level", path)
+	
+	if level:
+		level.queue_free()
+		remove_child(level)
+	
+	if path != "":
+		level = load(path).instance()
+		add_child(level)
+	
+	for team in get_children():
+		if team extends preload("res://player/team.gd"):
+			team.level = level
+			teams.push_back(team)
+
 func peer_connected(id):
 	print(id, " connected!")
+	
 	for pp in peer_players:
 		var team_name = peer_players[pp].get_parent().get_name()
 		rpc_id(id, "make_player", team_name, pp)
 	
-	for checkpoint in level.get_checkpoints():
-		checkpoint.update_status(true, id)
+	if level:
+		rpc_id(id, "load_level", level_path)
+		
+		for checkpoint in level.get_checkpoints():
+			checkpoint.update_status(true, id)
 	
 	var min_player_count = teams[0].get_player_count()
 	var min_team = teams[0]
--- game/menu/menu.gd
+++ game/menu/menu.gd
@@ -1,22 +1,47 @@
 extends Control
 
-enum Type {TYPE_SERVER, TYPE_CLIENT, TYPE_BOTH}
+enum Type {TYPE_CLIENT, TYPE_SERVER, TYPE_BOTH}
+class Level: 
+	var path = ""
+	var name = ""
+	func _init(_path = "", _name = "Unnamed level"): path = _path; name = _name
 
-export(NodePath) var type_select_
-export(NodePath) var host_field_
-export(NodePath) var host_label_
-export(NodePath) var max_players_field_
-export(NodePath) var max_players_label_
-export(NodePath) var port_field_
+export(NodePath) var grid_ = @"panel/parts/grid"
 
-onready var type_select = get_node(type_select_)
-onready var host_field = get_node(host_field_)
-onready var host_label = get_node(host_label_)
-onready var max_players_field = get_node(max_players_field_)
-onready var max_players_label = get_node(max_players_label_)
-onready var port_field = get_node(port_field_)
+onready var grid = get_node(grid_)
+onready var type_select = grid.get_node(@"type")
+onready var host_field = grid.get_node(@"host")
+onready var host_label = grid.get_node(@"host_label")
+onready var max_players_field = grid.get_node(@"max_players")
+onready var max_players_label = grid.get_node(@"max_players_label")
+onready var port_field = grid.get_node(@"port")
+onready var level_select = grid.get_node(@"level")
+onready var level_label = grid.get_node(@"level_label")
+
+var levels = []
 
 func _ready():
+	var dir = Directory.new()
+	var level_dirs_left = ["res://levels", "user://levels"]
+	while level_dirs_left.size() > 0:
+		var open_result = dir.open(level_dirs_left[-1])
+		level_dirs_left.pop_back()
+		if open_result == OK:
+			dir.list_dir_begin()
+			while true:
+				var file_name = dir.get_next()
+				if file_name == "":
+					break
+				var file_path = dir.get_current_dir().plus_file(file_name)
+				if dir.current_is_dir() and file_name != "." and file_name != "..":
+					level_dirs_left.push_back(file_path)
+				if file_name.match("level-*.?scn") or file_name.match("level-*.scn"):
+					levels.push_back(Level.new(file_path, file_name.replace("level-","").basename()))
+			dir.list_dir_end()
+		
+	for i in range(levels.size()):
+		level_select.add_item(levels[i].name, i)
+	
 	for arg in OS.get_cmdline_args():
 		if arg == "-c":
 			type_select.select(0)
@@ -28,6 +53,7 @@ func _ready():
 		elif arg == "-sc":
 			type_select.select(2)
 			start()
+	
 	type_select.select(2)
 	type_changed(type_select.get_selected_ID())
 	type_select.connect("item_selected", self, "type_changed")
@@ -37,13 +63,14 @@ func start():
 	var host = host_field.get_text()
 	var port = int(port_field.get_text())
 	var max_players = int(max_players_field.get_text())
-	_setup_tree(type, host, port, max_players)
+	var level = levels[level_select.get_selected_ID()]
+	_setup_tree(type, host, port, max_players, level.path)
 
 func start_and_duplicate():
 	start()
 	OS.execute(OS.get_executable_path(), OS.get_cmdline_args(), false)
 
-func _setup_tree(type, host, port, max_players, tree = get_tree()):
+func _setup_tree(type, host, port, max_players, level_path, tree = get_tree()):
 	var multiplayer = NetworkedMultiplayerENet.new()
 	var error = OK
 	if type == TYPE_SERVER:
@@ -56,17 +83,26 @@ func _setup_tree(type, host, port, max_players, tree = get_tree()):
 			var scenetree_holder = preload("scenetree_holder.gd").new()
 			scenetree_holder.set_name("server_tree_holder")
 			get_tree().get_root().add_child(scenetree_holder)
-			_setup_tree(TYPE_SERVER, host, port, max_players, scenetree_holder.scene_tree)
+			_setup_tree(TYPE_SERVER, host, port, max_players, level_path, scenetree_holder.scene_tree)
 	
 	if error:
 		print("Couldn't create server/client! Error: ", error)
 	else:
 		tree.set_network_peer(multiplayer)
 		tree.set_meta("network_peer", multiplayer)
-	tree.change_scene_to(preload("res://main/main.tscn"))
+	
+	var game_node = preload("res://main/main.tscn").instance()
+	tree.get_root().add_child(game_node)
+	if tree.get_current_scene():
+		tree.get_current_scene().queue_free()
+	tree.set_current_scene(game_node)
+	if type == TYPE_SERVER:
+		game_node.load_level(level_path)
 
 func type_changed(type):
 	host_field.set_hidden(type == TYPE_SERVER)
 	host_label.set_hidden(type == TYPE_SERVER)
 	max_players_field.set_hidden(type == TYPE_CLIENT)
 	max_players_label.set_hidden(type == TYPE_CLIENT)
+	level_select.set_hidden(type == TYPE_CLIENT)
+	level_label.set_hidden(type == TYPE_CLIENT)
